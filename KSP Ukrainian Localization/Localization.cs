62f2cefe01b6d8a5407eaf237ec7c84e75d6c9da
Oleksandr Pozdniakov <BeardVis@outlook.com>
2025-09-13 12:05:24 +0300

    Add project files.

---
+++ KSP Ukrainian Localization/Localization.cs
@@ -0,0 +1,25 @@
+﻿using KSP.Localization;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+namespace KSP_Ukrainian_Localization
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, false)]
+    public class Localization : MonoBehaviour
+    {
+        public void Awake()
+        {
+            if (Application.isPlaying)
+            {
+                FontLoader fontLoader = FindObjectOfType<FontLoader>();
+                TMP_FontAsset fontAsset = fontLoader.LoadedFonts.FirstOrDefault(asset => asset.name.Equals("play"));
+                fontLoader.AddMenuSubFont("uk", false, new[] { fontAsset });
+                fontLoader.SwitchFontLanguage("uk");
+
+                Localizer.SwitchToLanguage("uk");
+
+            }
+        }
+    }
+}
